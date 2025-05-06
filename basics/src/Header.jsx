09eb96629b1b56538aa3c2ceea6427b8df430cd4
vishnukannanklk <vishnukannan1999klk@gmail.com>
2025-05-06 21:25:22 +0530

    props concept

---
+++ basics/src/Header.jsx
@@ -0,0 +1,9 @@
+function Header(props){
+    return(
+        <>
+        <h1> Welcome {props.name} {props.age}</h1>
+        <header>Some header </header>
+        </>
+    )
+}
+export default Header;
---
+++ basics/src/greeting.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Greeting(props) {
+    return(
+        <h1>Hello, {props.name}</h1>
+
+    );
+}
+
+export default Greeting;
